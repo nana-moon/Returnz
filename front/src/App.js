23527da022779c0133aacdac1c7c23a74f8f0091
jiaekim <jiae1625@gmail.com>
2023-03-14 10:08:30 +0900

    feat: front setting

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Outlet } from 'react-router-dom';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+function App() {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
---
+++ front/src/index.js
@@ -0,0 +1,41 @@
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
+import Waiting from './pages/Waiting';
+import Game from './pages/Game';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Main /> },
+      { path: '/waiting', element: <Waiting /> },
+      { path: '/game', element: <Game /> },
+    ],
+  },
+]);
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
