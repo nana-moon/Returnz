cc70ff1e7030ef87cc34bba07ea4a637eb5c37d9
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-03-14 16:31:05 +0900

    init: common css 추가

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import tw, { styled } from 'twin.macro';
 
 export default function Main() {
-  return <MainDiv>Main</MainDiv>;
+  return <MainDiv>Returnz</MainDiv>;
 }
 
 const MainDiv = styled.div`
-  ${tw`border-2 text-primary`}
+  ${tw`border-2 text-primary font-ibm font-bold`}
 `;
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,17 +1,26 @@
 /* eslint-disable prettier/prettier */
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      'primary':'#1CD6C9',
-      'base':'#F8FAFC',
-      'negative':'#D4D4D4',
-      'gain':'#FF5454',
-      'lose':'#556BD5',
-      'input':'#EDEEFF',
+    extend: {
+      colors: {
+        'primary':'#1CD6C9',
+        'base':'#F8FAFC',
+        'negative':'#D4D4D4',
+        'gain':'#FF5454',
+        'lose':'#556BD5',
+        'input':'#EDEEFF',
+        // 메달 색깔
+        'gold': '#EBD516',
+        'silver': '#A7A7A7',
+        'bronze': '#948150',
+      },
     },
-    extend: {},
+    fontFamily: {
+      spoq: ['Spoqa Han Sans Neo', 'sans-serif'],
+      ibm: ['IBM-Plex-Mono', 'sans-serif']
+    },
   },
   plugins: [],
 };
