6494de84f1f948b4772cc67cce1a86a0ab5484b8
문여경 <logbymoon@naver.com>
2023-03-14 12:07:50 +0900

    back setting

---
+++ back/src/main/java/bunsan/returnz/ReturnzApplication.java
@@ -0,0 +1,13 @@
+package bunsan.returnz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnzApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/bunsan/returnz/ReturnzApplicationTests.java
@@ -0,0 +1,13 @@
+package bunsan.returnz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
