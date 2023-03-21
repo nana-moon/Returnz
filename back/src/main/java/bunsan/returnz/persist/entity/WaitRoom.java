673df1bb64d0fe2887fec9a9ef12c62112263cc0
ManduTheCat <krocd@naver.com>
2023-03-21 14:15:27 +0900

    feat: add WaitRoom 엔티티 추가

---
+++ back/src/main/java/bunsan/returnz/persist/entity/WaitRoom.java
@@ -0,0 +1,19 @@
+package bunsan.returnz.persist.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+
+@Entity
+@Getter
+public class WaitRoom {
+	@GeneratedValue
+	@Id
+	@Column(name="WAIT_ROOM_ID")
+	Long id;
+	String waitingTip;
+}
