4a218b446b15a9cc4ac2a890560eb5aa3ea85e61
Fortuna3Co <Fortuna3Co@gmail.com>
2023-03-23 10:34:27 +0900

    feat : Entity 생성, 수정
    Former-commit-id: e52f45a9412b877912f3f7b62983de38b3ffae93

--- back/src/main/java/bunsan/returnz/persist/entity/GradingHistory.java
+++ back/src/main/java/bunsan/returnz/persist/entity/GradingHistory.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,7 +27,7 @@ public class GradingHistory {
 	@Column(name = "GRADING_HISTORY_ID")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "COMAPNY_CODE")
 	private Company company;
 
