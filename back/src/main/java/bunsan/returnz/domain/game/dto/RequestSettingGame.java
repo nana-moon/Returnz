33d5565d41217f38eb149c1ee80d0e431e2a439c
ManduTheCat <krocd@naver.com>
2023-03-23 14:20:13 +0900

    init: 게임 생성 컨트롤러

---
+++ back/src/main/java/bunsan/returnz/domain/game/dto/RequestSettingGame.java
@@ -0,0 +1,14 @@
+package bunsan.returnz.domain.game.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class RequestSettingGame {
+	private String theme;
+	//---- 사용자 설정일때만 아래 값을 사용합니다 -----
+	private String ternPerTime;
+	private LocalDate startTime;
+	private String totalTurn;
+	private List<String> memberIdList;
+
+}
