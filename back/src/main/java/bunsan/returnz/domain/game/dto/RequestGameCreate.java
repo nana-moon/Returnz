62cd32e8f0146620b0617b8280a37ed8769ad2b3
ManduTheCat <krocd@naver.com>
2023-03-23 10:37:55 +0900

    init: 게임 생성 컨트롤러

---
+++ back/src/main/java/bunsan/returnz/domain/game/dto/RequestGameCreate.java
@@ -0,0 +1,10 @@
+package bunsan.returnz.domain.game.dto;
+
+import java.util.List;
+
+public class RequestGameCreate {
+	private String theme;
+	private String ternPerTime;
+	private String totalCount;
+	private List<String> memberIdList;
+}
