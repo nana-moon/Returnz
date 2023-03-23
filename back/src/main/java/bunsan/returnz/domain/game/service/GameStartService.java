284b85b9bff91bdef40b4829b4544d7371860671
ManduTheCat <krocd@naver.com>
2023-03-24 08:43:58 +0900

    feature: 게임 생성 설정 엔티티에 할당
    Former-commit-id: ad8a4ed76cee994403d293bdd4c1b94bdee12196

--- back/src/main/java/bunsan/returnz/domain/game/service/GameStartService.java
+++ back/src/main/java/bunsan/returnz/domain/game/service/GameStartService.java
@@ -16,9 +16,13 @@ import bunsan.returnz.domain.game.dto.RequestSettingGame;
 import bunsan.returnz.persist.entity.Company;
 import bunsan.returnz.persist.entity.GameRoom;
 import bunsan.returnz.persist.entity.GameStock;
+import bunsan.returnz.persist.entity.Gamer;
+import bunsan.returnz.persist.entity.Member;
 import bunsan.returnz.persist.repository.CompanyRepository;
 import bunsan.returnz.persist.repository.GameRoomRepository;
 import bunsan.returnz.persist.repository.GameStockRepository;
+import bunsan.returnz.persist.repository.GamerRepository;
+import bunsan.returnz.persist.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,8 +30,11 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 @Slf4j
 public class GameStartService {
+	private final GamerRepository gamerRepository;
 	private final GameRoomRepository gameRoomRepository;
 	private final GameStockRepository gameStockRepository;
+
+	private final MemberRepository memberRepository;
 	CompanyRepository companyRepository;
 	@Transactional
 	public Map<String, Object> settingGame(RequestSettingGame requestSettingGame) {
@@ -42,34 +49,34 @@ public class GameStartService {
 			.turnPerTime(requestSettingGame.getTernPerTime())
 			.build();
 		// 랜덤 주식 가져와서 할당하기
+		gameRoomRepository.save(newGameRoom);
 		Pageable pageable = PageRequest.of(0, 10);
 		// TODO: 2023/03/23 일단 랜덤 조건을 달아야한다면 전체 긁어와서 처리해야한다. ex 시가 총액
 		Page<Company> randomCompaniesPage = companyRepository.findRandomCompanies(pageable);
 		List<Company> CompanyList = randomCompaniesPage.getContent();
-		try{
-
-			for (Company company : CompanyList) {
-				GameStock companyEntity = GameStock.builder()
-					.companyName(company.getCompanyName())
-					.companyCode(company.getCode())
-					.gameRoom(newGameRoom)
-					.build();
-				gameStockRepository.save(companyEntity);
-			}
+		for (Company company : CompanyList) {
+			GameStock companyEntity = GameStock.builder()
+				.companyName(company.getCompanyName())
+				.companyCode(company.getCode())
+				.gameRoom(newGameRoom)
+				.build();
+			gameStockRepository.save(companyEntity);
 		}
-		catch (Exception e){
-			e.printStackTrace();
-		}
-		gameRoomRepository.save(newGameRoom);
 		// 맴버 가져와서 주식방 게이머 에 할당하기
 		log.info(requestSettingGame.getMemberIdList().toString());
-
+		List<Member> getMemberId = memberRepository.findAllById(requestSettingGame.getMemberIdList());
+		for (Member member : getMemberId) {
+			Gamer gamer = Gamer.builder()
+				.id(member.getId())
+				.gameRoom(newGameRoom)
+				.deposit(100000)// TODO: 2023/03/24 이거 설정 결정되면 처리해야함
+				.username(member.getUsername())
+				.build();
+			gamerRepository.save(gamer);
+		}
 		// 게이머가 가진 주식 할당하기
 
 
-
-
-		// 랜덤 주식 게임 방 세팅
 		return null;
 	}
 
