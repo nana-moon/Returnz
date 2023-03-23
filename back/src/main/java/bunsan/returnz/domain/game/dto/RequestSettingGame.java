1dd6f33c9be0368f6d982bb04c5e48b3e6032dab
ManduTheCat <krocd@naver.com>
2023-03-23 22:19:12 +0900

    fix: validation dto 로 이동

--- back/src/main/java/bunsan/returnz/domain/game/dto/RequestSettingGame.java
+++ back/src/main/java/bunsan/returnz/domain/game/dto/RequestSettingGame.java
@@ -2,6 +2,7 @@ package bunsan.returnz.domain.game.dto;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -10,6 +11,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import bunsan.returnz.domain.game.enums.Theme;
 import bunsan.returnz.domain.game.enums.TurnPerTime;
+import bunsan.returnz.global.advice.exception.BadRequestException;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -29,5 +31,35 @@ public class RequestSettingGame {
 	@Nullable
 	private Integer totalTurn;
 	private List<String> memberIdList;
+
+	public void validateRequestSettingGame() {
+
+		if (this.theme.getTheme().equals("USER")) {
+			// 세게중 하나라도 널이면 안된다.
+			if ((this.totalTurn == null
+				|| this.startTime == null
+				||this.ternPerTime == null
+			)) {
+				throw new BadRequestException("사용자 태마는 총턴수, 시작 일,시, 턴당 시간 을 지정하세요");
+			}
+			// 시작일로 부터 현재까지 남은 턴수
+			long weekBetweenTurn = ChronoUnit.WEEKS.between(this.startTime, LocalDateTime.now());
+			long monthsBetweenTurn = ChronoUnit.MONTHS.between(this.startTime, LocalDateTime.now());
+			long daysBetweenTurn = ChronoUnit.DAYS.between(this.startTime, LocalDateTime.now());
+			if (this.ternPerTime.getTime().equals("DAY")
+				&& this.totalTurn > daysBetweenTurn) {
+				throw new BadRequestException("시작일수로 부터 가능한 일 이 더 작습니다. 계산된 일 "+ daysBetweenTurn);
+			}
+			if (this.ternPerTime.getTime().equals("WEEK")
+				&& this.totalTurn > weekBetweenTurn) {
+				throw new BadRequestException("시작일수로 부터 가능한 주 가 더 작습니다. 계산된 주 " + weekBetweenTurn);
+			}
+			if (this.ternPerTime.getTime().equals("MONTH")
+				&& this.totalTurn > monthsBetweenTurn) {
+				throw new BadRequestException("시작일수로 부터 가능한 달 이 더 작습니다. 계산된 달 " + monthsBetweenTurn);
+			}
+
+		}
+	}
 
 }
--- back/src/main/java/bunsan/returnz/domain/game/enums/Theme.java
+++ back/src/main/java/bunsan/returnz/domain/game/enums/Theme.java
@@ -35,33 +35,4 @@ public enum Theme {
 	}
 
 	// TODO: 2023/03/23 이거 디티오에 있어야하는거 같은데요?
-	public void validateRequestSettingGame(RequestSettingGame requestSettingGame) {
-
-		if (requestSettingGame.getTheme().getTheme().equals("USER")) {
-			// 세게중 하나라도 널이면 안된다.
-			if ((requestSettingGame.getTotalTurn() == null
-				|| requestSettingGame.getStartTime() == null
-				|| requestSettingGame.getTernPerTime() == null
-			)) {
-				throw new BadRequestException("사용자 태마는 총턴수, 시작 일,시, 턴당 시간 을 지정하세요");
-			}
-			// 시작일로 부터 현재까지 남은 턴수
-			long weekBetweenTurn = ChronoUnit.WEEKS.between(requestSettingGame.getStartTime(), LocalDateTime.now());
-			long monthsBetweenTurn = ChronoUnit.MONTHS.between(requestSettingGame.getStartTime(), LocalDateTime.now());
-			long daysBetweenTurn = ChronoUnit.DAYS.between(requestSettingGame.getStartTime(), LocalDateTime.now());
-			if (requestSettingGame.getTernPerTime().getTime().equals("DAY")
-				&& requestSettingGame.getTotalTurn() > daysBetweenTurn) {
-				throw new BadRequestException("시작일수로 부터 가능한 일 이 더 작습니다. 계산된 일 "+ daysBetweenTurn);
-			}
-			if (requestSettingGame.getTernPerTime().getTime().equals("WEEK")
-				&& requestSettingGame.getTotalTurn() > weekBetweenTurn) {
-				throw new BadRequestException("시작일수로 부터 가능한 주 가 더 작습니다. 계산된 주 " + weekBetweenTurn);
-			}
-			if (requestSettingGame.getTernPerTime().getTime().equals("MONTH")
-				&& requestSettingGame.getTotalTurn() > monthsBetweenTurn) {
-				throw new BadRequestException("시작일수로 부터 가능한 달 이 더 작습니다. 계산된 달 " + monthsBetweenTurn);
-			}
-
-		}
-	}
 }
