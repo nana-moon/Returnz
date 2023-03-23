816809524d185b2cbbf16c4e0ec43e1c82878ae0
Fortuna3Co <Fortuna3Co@gmail.com>
2023-03-23 10:02:58 +0900

    etc : file add
    Former-commit-id: 5978885d5f7abc0d7573ddd64e1acfc3b3e4055b

---
+++ back/src/main/java/bunsan/returnz/persist/entity/HistoricalPriceMinute.java
@@ -0,0 +1,41 @@
+package bunsan.returnz.persist.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HistoricalPrice {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "HISTORICAL_PRICE_ID")
+	private Long id;
+
+	private LocalDateTime dateTime;
+	private double open;
+	private double high;
+	private double low;
+	private double close;
+	private double volume;
+	private double adjclose;
+	private double dividends;
+
+	@ManyToOne
+	@JoinColumn(name = "COMPANY_CODE")
+	private Company company;
+}
