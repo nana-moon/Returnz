bf7dc0e8b3f70a5c21ce42247865dc8c01077768
Fortuna3Co <Fortuna3Co@gmail.com>
2023-03-24 11:05:20 +0900

    feat : game controller, service, dto 생성 및 수정

--- back/src/main/java/bunsan/returnz/persist/repository/GamerRepository.java
+++ back/src/main/java/bunsan/returnz/persist/repository/GamerRepository.java
@@ -1,8 +1,11 @@
 package bunsan.returnz.persist.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import bunsan.returnz.persist.entity.Gamer;
 
 public interface GamerRepository extends JpaRepository<Gamer, Long> {
+	List<Gamer> findAllBygameRoomId(Long id);
 }
