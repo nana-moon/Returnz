5a2b64f667073b7ffe590eb1bf59deef59432b94
Fortuna3Co <Fortuna3Co@gmail.com>
2023-03-24 11:06:04 +0900

    feat : game controller, service, dto 생성 및 수정
    Former-commit-id: 36286c5686a07763c57637deb92f17ad52f9bac8

--- back/src/main/java/bunsan/returnz/domain/game/dto/GameGamerDto.java
+++ back/src/main/java/bunsan/returnz/domain/game/dto/GameGamerDto.java
@@ -1,6 +1,16 @@
 package bunsan.returnz.domain.game.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class GameGamerDto {
+	private Long gamerId;
 	private String userName;
 	private Integer deposit;
 	private Integer totalBuyAmoount;
