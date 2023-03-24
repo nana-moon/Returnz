697619c1db6c3292206b3f7ae97a59e076fe94b1
jiaekim <jiae1625@gmail.com>
2023-03-24 13:35:05 +0900

    makeroom Api

--- front/src/pages/WaitingPage.jsx
+++ front/src/pages/WaitingPage.jsx
@@ -1,40 +1,72 @@
-import React, { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import tw, { styled } from 'twin.macro';
 import { Link } from 'react-router-dom';
 import Chatting from '../components/chatting/Chatting';
 import ThemeSetting from '../components/waiting/ThemeSetting';
 import UserSetting from '../components/waiting/UserSetting';
 import WaitingListItem from '../components/waiting/WaitingListItem';
+import { makeRoomApi } from '../apis/gameApi';
 
 export default function WaitingPage() {
+  // 방장
   const [isHost, setIsHost] = useState(true);
+
+  // 대기자 리스트
   const [userList, setUserList] = useState([
     { id: 1, profile: 'green.jpg', nickname: 'chat혜성', profit: '28.7' },
     { id: 2 },
     { id: 3 },
     { id: 4 },
   ]);
-  const [theme, setTheme] = useState(false);
-  const [userSetting, setUserSetting] = useState(false);
-  const [isUserSetting, setIsUserSetting] = useState(false);
 
-  // 테마 설정 관련
+  // 게임 설정 state
+  const initial = {
+    theme: null,
+    turnPerTime: 'NO',
+    startTime: null,
+    totalTurn: null,
+    memberIdList: [],
+  };
+  const [setting, setSetting] = useState(initial);
+  const [isUserSetting, setIsUserSetting] = useState(false); // 사용자 설정 확인
+  const [isValidSetting, setIsValidSetting] = useState(true); // 설정 유효성 검사
+
+  // 게임 설정 action
+  const isValid = () => {
+    if (setting.theme === null) {
+      return false;
+    }
+    if (setting.theme === 'usersetting') {
+      const flag = () => setting.some((set) => set === 'NO' || set === null);
+      return !flag;
+    }
+    return true;
+  };
   const getIsUserSetting = () => {
     setIsUserSetting(!isUserSetting);
   };
   const getTheme = (data) => {
-    setTheme(data);
+    const newData = { ...setting, theme: data };
+    setSetting(newData);
+    console.log(setting, 'setting');
+
+    console.log(isValidSetting);
   };
-  const getUserSetting = (data) => {
-    setUserSetting(data);
+  const getUserSetting = (newData) => {
+    setSetting(newData);
+  };
+  useEffect(() => {
+    const flag = isValid();
+    setIsValidSetting(flag);
+    console.log(isValidSetting);
+  }, [setting]);
+
+  // 게임 시작
+  const handleStart = async (e) => {
+    makeRoomApi(setting);
   };
 
-  // 게임 준비 및 시작 관련
-  const handleStart = (e) => {
-    if (!theme && !userSetting) {
-      e.preventDefault();
-    }
-  };
   return (
     <WaitingContainer>
       <WaitingListSection>
@@ -43,19 +75,17 @@ export default function WaitingPage() {
         })}
       </WaitingListSection>
       <SettingSection>
-        {!isUserSetting && <ThemeSetting getIsUser={getIsUserSetting} getTheme={getTheme} />}
-        {isUserSetting && <UserSetting getIsUser={getIsUserSetting} getUserSetting={getUserSetting} />}
+        {!isUserSetting && <ThemeSetting getIsUserSetting={getIsUserSetting} getTheme={getTheme} />}
+        {isUserSetting && (
+          <UserSetting setting={setting} getIsUserSetting={getIsUserSetting} getUserSetting={getUserSetting} />
+        )}
         <ChatSection>
           <Chatting />
           <BtnBox>
-            {isHost && (
-              <Button to="/game" onClick={handleStart} className="bg-primary hover:bg-dprimary">
-                시작하기
-              </Button>
-            )}
-            <Button to="/" className="bg-[#E19999] hover:bg-[#976161]">
+            {isHost && <StartButton onClick={handleStart}>시작하기</StartButton>}
+            <BackButton to="/" className="bg-[#E19999] hover:bg-[#976161]">
               나가기
-            </Button>
+            </BackButton>
           </BtnBox>
         </ChatSection>
       </SettingSection>
@@ -78,6 +108,10 @@ const ChatSection = styled.section`
 const BtnBox = styled.div`
   ${tw`flex gap-5 mt-5`}
 `;
-const Button = styled(Link)`
+const StartButton = styled.button`
+  ${tw`border rounded-xl w-[50%] min-h-[50px] flex justify-center items-center text-white text-xl font-bold transition-colors`}
+  ${({ disabled }) => (disabled ? tw`bg-primary hover:bg-none` : tw`bg-primary hover:bg-dprimary`)}
+`;
+const BackButton = styled(Link)`
   ${tw`border rounded-xl w-[50%] min-h-[50px] flex justify-center items-center text-white text-xl font-bold transition-colors`}
 `;
