fe58992a9b04d5997d111dde0464e4a837ae455c
ManduTheCat <krocd@naver.com>
2023-03-24 12:01:12 +0900

    fix: 게이머 스톡 이 한유저만 저장되는 문제 GamerStock id 추가 해서 해결
    Former-commit-id: 6bb78967148a9cd88f7486872d9f2df6783b9b2d

--- back/src/main/java/bunsan/returnz/domain/game/service/GameStartService.java
+++ back/src/main/java/bunsan/returnz/domain/game/service/GameStartService.java
@@ -1,6 +1,8 @@
 package bunsan.returnz.domain.game.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -13,15 +15,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import bunsan.returnz.domain.game.dto.RequestSettingGame;
+import bunsan.returnz.global.advice.exception.BadRequestException;
 import bunsan.returnz.persist.entity.Company;
 import bunsan.returnz.persist.entity.GameRoom;
 import bunsan.returnz.persist.entity.GameStock;
 import bunsan.returnz.persist.entity.Gamer;
+import bunsan.returnz.persist.entity.GamerStock;
 import bunsan.returnz.persist.entity.Member;
 import bunsan.returnz.persist.repository.CompanyRepository;
 import bunsan.returnz.persist.repository.GameRoomRepository;
 import bunsan.returnz.persist.repository.GameStockRepository;
 import bunsan.returnz.persist.repository.GamerRepository;
+import bunsan.returnz.persist.repository.GamerStockRepository;
 import bunsan.returnz.persist.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,9 +38,10 @@ public class GameStartService {
 	private final GamerRepository gamerRepository;
 	private final GameRoomRepository gameRoomRepository;
 	private final GameStockRepository gameStockRepository;
-
+	private final GamerStockRepository gamerStockRepository;
 	private final MemberRepository memberRepository;
 	CompanyRepository companyRepository;
+
 	@Transactional
 	public Map<String, Object> settingGame(RequestSettingGame requestSettingGame) {
 		// 주식방 만들기
@@ -43,18 +49,16 @@ public class GameStartService {
 			.roomId(UUID.randomUUID().toString())
 			.turnPerTime(requestSettingGame.getTernPerTime())
 			.theme(requestSettingGame.getTheme())
-			.curDate(LocalDateTime.now())
+			.curDate(requestSettingGame.getThemeStartTime())
 			.totalTurn(requestSettingGame.getTotalTurn())
 			.roomMemberCount(requestSettingGame.getMemberIdList().size())
-			.turnPerTime(requestSettingGame.getTernPerTime())
 			.build();
 		// 랜덤 주식 가져와서 할당하기
 		gameRoomRepository.save(newGameRoom);
 		Pageable pageable = PageRequest.of(0, 10);
-		// TODO: 2023/03/23 일단 랜덤 조건을 달아야한다면 전체 긁어와서 처리해야한다. ex 시가 총액
 		Page<Company> randomCompaniesPage = companyRepository.findRandomCompanies(pageable);
-		List<Company> CompanyList = randomCompaniesPage.getContent();
-		for (Company company : CompanyList) {
+		List<Company> companyList = randomCompaniesPage.getContent();
+		for (Company company : companyList) {
 			GameStock companyEntity = GameStock.builder()
 				.companyName(company.getCompanyName())
 				.companyCode(company.getCode())
@@ -65,19 +69,37 @@ public class GameStartService {
 		// 맴버 가져와서 주식방 게이머 에 할당하기
 		log.info(requestSettingGame.getMemberIdList().toString());
 		List<Member> getMemberId = memberRepository.findAllById(requestSettingGame.getMemberIdList());
+		if (getMemberId.size() == 0) {
+			throw new BadRequestException("유효한 참가자 아이디가 아닙니다");
+		}
+		List<Gamer> gamers = new ArrayList<>();
 		for (Member member : getMemberId) {
 			Gamer gamer = Gamer.builder()
-				.id(member.getId())
+				.memberId(member.getId())
 				.gameRoom(newGameRoom)
-				.deposit(100000)// TODO: 2023/03/24 이거 설정 결정되면 처리해야함
+				.deposit(100000000)
 				.username(member.getUsername())
 				.build();
+			gamers.add(gamer);
 			gamerRepository.save(gamer);
 		}
 		// 게이머가 가진 주식 할당하기
+		// 게임 스톡을 만들면서 게이머를 할당해야한다
+		// 게이머들순회해서 아이디 가져오고
+		for (Gamer gamer : gamers) {
+			for (Company company : companyList) {
+				GamerStock gamerStock = GamerStock.builder()
+					.companyCode(company.getCode())
+					.gamer(gamer)
+					.build();
+				GamerStock save = gamerStockRepository.save(gamerStock);
+			}
+		}
 
-
-		return null;
+		Map<String, Object> gameRoomsRes = new HashMap<>();
+		gameRoomsRes.put("roomId", newGameRoom.getRoomId());
+		gameRoomsRes.put("id", newGameRoom.getId());
+		return gameRoomsRes;
 	}
 
 }
