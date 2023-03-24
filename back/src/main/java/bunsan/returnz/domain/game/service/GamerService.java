109b9bb9531d418444058cc3d632e8e34c586fe5
Fortuna3Co <Fortuna3Co@gmail.com>
2023-03-24 11:05:15 +0900

    feat : game controller, service, dto 생성 및 수정

---
+++ back/src/main/java/bunsan/returnz/domain/game/service/GamerService.java
@@ -0,0 +1,43 @@
+package bunsan.returnz.domain.game.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import bunsan.returnz.domain.game.dto.GameGamerDto;
+import bunsan.returnz.persist.entity.Gamer;
+import bunsan.returnz.persist.repository.GamerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class GamerService {
+	private final GamerRepository gamerRepository;
+
+	public List<GameGamerDto> findAllBygameRoomId(Long id) {
+		List<Gamer> gamerList = gamerRepository.findAllBygameRoomId(id);
+		// TODO: gamerList가 empty면 에러 발생
+		// if(gamerList.isEmpty())
+
+		Gamer gamer = new Gamer();
+		List<GameGamerDto> gameGamerDtos = new ArrayList<>();
+		for (int i = 0; i < gamerList.size(); ++i) {
+			gameGamerDtos.add(gamer.toDto(gamerList.get(i)));
+		}
+		return gameGamerDtos;
+	}
+
+	public GameGamerDto findById(Long gamerId) {
+		Optional<Gamer> gamerOptional = gamerRepository.findById(gamerId);
+		if (gamerOptional.isPresent()) {
+			Gamer game = new Gamer();
+			return game.toDto(gamerOptional.get());
+		}
+		// TODO : else
+		return null;
+	}
+}
