98772d413401d306f8abed41838bfa3ec460ca5d
nana-moon <yeokyung502@gmail.com>
2023-03-27 21:34:32 +0900

    fix: MR 피드백 반영
    Former-commit-id: 0d7bac1c128769acda0a9a7cb02b50aed3991654

--- back/src/main/java/bunsan/returnz/persist/entity/TodayWord.java
+++ back/src/main/java/bunsan/returnz/persist/entity/TodayWord.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import bunsan.returnz.domain.mainpage.dto.TodayWordDto;
 import lombok.AllArgsConstructor;
@@ -26,7 +27,7 @@ public class TodayWord {
 	private Integer id;
 	private String topic;
 	private String word;
-	@Column(length = 10000)
+	@Lob
 	private String description;
 
 	public TodayWordDto toDto() {
--- back/src/main/java/bunsan/returnz/persist/repository/EconomicWordRepository.java
+++ back/src/main/java/bunsan/returnz/persist/repository/EconomicWordRepository.java
@@ -1,14 +1,17 @@
 package bunsan.returnz.persist.repository;
 
+
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import bunsan.returnz.persist.entity.EconomicWord;
 
 public interface EconomicWordRepository extends JpaRepository<EconomicWord, Long> {
-	@Query(value = "SELECT * FROM yg_db.economic_word order by RAND() limit 10", nativeQuery = true)
-	List<EconomicWord> findByRandom();
+	@Query(value = "SELECT e FROM EconomicWord e ORDER BY RAND()")
+	Page<EconomicWord> findRandomWords(Pageable pageable);
 
 }
