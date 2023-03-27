7f20eb3cf082631e9464058ed97c2a80f76f07ac
ats166 <ats166@naver.com>
2023-03-27 17:45:16 +0900

    feature : 첫턴 상장종목 api 요청 성공
    Former-commit-id: ce387fbde675e935142ba6c4030d1b79a411909d

---
+++ front/src/store/gamedata/GameData.selector.js
@@ -0,0 +1,8 @@
+// 상장종목, 그래프
+export const stockDataList = (state) => state.gamedatas.stockDataList;
+// 뉴스, 주가정보, 종목 내용
+export const stockDetailDataList = (state) => state.gamedatas.stockDetailDataList;
+// 보유 종목
+export const gamerStockList = (state) => state.gamedatas.gamerStockList;
+//  유저 데이터
+export const gamerDataList = (state) => state.gamedatas.gamerDataList;
