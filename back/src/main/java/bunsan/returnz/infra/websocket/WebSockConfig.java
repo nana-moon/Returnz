cee6a27bc98297d0a07259ed5979a34e32d4e3ce
문여경 <logbymoon@naver.com>
2023-03-28 11:09:49 +0900

    hotfix: CrossOrigin import 추가

--- back/src/main/java/bunsan/returnz/infra/websocket/WebSockConfig.java
+++ back/src/main/java/bunsan/returnz/infra/websocket/WebSockConfig.java
@@ -3,6 +3,7 @@ package bunsan.returnz.infra.websocket;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
