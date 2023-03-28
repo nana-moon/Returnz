e930d953013199ccb8587de5675eda360bd1ad81
ats166 <ats166@naver.com>
2023-03-28 15:41:51 +0900

    feature: 상장종목 전일대비 계산
    Former-commit-id: 546d7b8a1118e3118992aca41cd3b3ac841bc95f

--- front/src/store/gamedata/GameData.reducer.js
+++ front/src/store/gamedata/GameData.reducer.js
@@ -4,8 +4,13 @@ import { createSlice } from '@reduxjs/toolkit';
 export const gamedata = createSlice({
   name: 'gamedata',
   initialState: {
-    // 상장종목, 그래프
+    // 상장종목
     stockDataList: {},
+    // 그래프
+    stockGraphList: {
+      candledata: [],
+      linedata: [],
+    },
     // 뉴스, 주가정보, 종목 내용
     stockDetailDataList: {},
     // 보유종목
@@ -19,7 +24,13 @@ export const gamedata = createSlice({
       state.stockDataList = action.payload;
     },
     // 두번째 부터
-    handleMoreGameData(state, action) {},
+    handleMoreGameData(state, action) {
+      const keys = Object.keys(state.stockDataList);
+
+      for (const key of keys) {
+        state.stockDataList[key] = [...state.stockDataList[key], ...action.payload[key]];
+      }
+    },
     // 뉴스, 주가정보, 종목 내용
     handleGetGameDetailData(state, action) {},
   },
