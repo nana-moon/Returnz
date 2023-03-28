6d617dff464901e5fb54640c316163ac3857ac86
ManduTheCat <krocd@naver.com>
2023-03-28 14:55:30 +0900

    feature: 애러 체크 완성
    Former-commit-id: 91715d0b37ab21892026c8bf91a63680ba68c468

--- back/src/main/java/bunsan/returnz/persist/repository/HistoricalPriceDayRepository.java
+++ back/src/main/java/bunsan/returnz/persist/repository/HistoricalPriceDayRepository.java
@@ -8,38 +8,47 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import bunsan.returnz.persist.entity.HistoricalPriceDay;
 
 public interface HistoricalPriceDayRepository extends JpaRepository<HistoricalPriceDay, Long> {
 	@Query(value = "SELECT * FROM historical_price_day h\n"
-		+ "WHERE h.date_time <= :dateTime AND h.company_code = :companyCode\n"
-		+ "ORDER BY h.date_time DESC\n"
+		+ "WHERE h.date_time <= :dateTime AND h.company_code = :companyCode\n" + "ORDER BY h.date_time DESC\n"
 		+ "LIMIT 20", nativeQuery = true)
-	List<HistoricalPriceDay> findAllByDateTimeIsBeforeWithCodeLimit20(LocalDateTime dateTime,
-		String companyCode);
+	List<HistoricalPriceDay> findAllByDateTimeIsBeforeWithCodeLimit20(LocalDateTime dateTime, String companyCode);
 
 	@Query(value = "SELECT * FROM historical_price_day h\n"
-		+ "WHERE h.date_time <= :dateTime AND h.company_code = :companyCode\n"
-		+ "ORDER BY h.date_time DESC\n"
+		+ "WHERE h.date_time <= :dateTime AND h.company_code = :companyCode\n" + "ORDER BY h.date_time DESC\n"
 		+ "LIMIT 1", nativeQuery = true)
-	List<HistoricalPriceDay> findAllByDateTimeIsBeforeWithCodeLimit1(LocalDateTime dateTime,
-		String companyCode);
+	List<HistoricalPriceDay> findAllByDateTimeIsBeforeWithCodeLimit1(LocalDateTime dateTime, String companyCode);
 
 	@Query(value = "SELECT * FROM historical_price_day h\n"
-		+ "WHERE h.date_time > :dateTime AND h.company_code = :companyCode\n"
-		+ "ORDER BY h.date_time ASC\n"
+		+ "WHERE h.date_time > :dateTime AND h.company_code = :companyCode\n" + "ORDER BY h.date_time ASC\n"
 		+ "LIMIT 1", nativeQuery = true)
-	Optional<HistoricalPriceDay> findByDateTimeIsAfterWithCodeLimit1(LocalDateTime dateTime,
-		String companyCode);
+	Optional<HistoricalPriceDay> findByDateTimeIsAfterWithCodeLimit1(LocalDateTime dateTime, String companyCode);
 
 	Optional<HistoricalPriceDay> findByDateTimeAndCompanyCode(LocalDateTime dateTime, String companyCode);
 
 	// day
 	// week
 	//month 별 턴처리 필요
-	@Query(value = "SELECT h FROM HistoricalPriceDay h WHERE h.dateTime > :dateTime AND h.company.code IN :stockIds ORDER BY h.dateTime ASC")
+	@Query(value = "SELECT h FROM HistoricalPriceDay h "
+		+ "WHERE h.dateTime > :dateTime "
+		+ "AND h.company.code IN :stockIds "
+		+ "ORDER BY h.dateTime ASC")
 	List<HistoricalPriceDay> getDayDataAfterStartDay(@Param("dateTime") LocalDateTime dateTime,
-		@Param("stockIds") List<String> stockIds,
-		Pageable pageable);
+		@Param("stockIds") List<String> stockIds, Pageable pageable);
+
+	// 해당기간내에 데이터가 몇개 있는지
+	@Query(value = "SELECT COUNT(DISTINCT h.company.code) = :stockCount FROM HistoricalPriceDay h "
+		+ "WHERE h.dateTime BETWEEN :startDate AND :endDate "
+		+ "AND h.company.code IN :stockIds")
+	boolean existsAtLeastOneRecordForEachCompany(@Param("startDate") LocalDateTime startDate,
+		@Param("endDate") LocalDateTime endDate, @Param("stockIds") List<String> stockIds,
+		@Param("stockCount") Long stockCount);
+
+
+
+
 }
