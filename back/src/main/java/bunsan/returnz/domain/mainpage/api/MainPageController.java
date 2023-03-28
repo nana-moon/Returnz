2e1b5084b5de31eb53b0f8a7d9877f2a89c6818d
문여경 <logbymoon@naver.com>
2023-03-28 23:09:44 +0900

    hotfix: CORS 에러 핸들링 Former-commit-id: 5c833c3581c6e50f660f341751fbcdb5e3663eda

--- back/src/main/java/bunsan/returnz/domain/mainpage/api/MainPageController.java
+++ back/src/main/java/bunsan/returnz/domain/mainpage/api/MainPageController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import bunsan.returnz.domain.mainpage.dto.TodayWordDto;
 import bunsan.returnz.domain.mainpage.service.MainPageService;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MainPageController {
 	public final MainPageService mainPageService;
 
