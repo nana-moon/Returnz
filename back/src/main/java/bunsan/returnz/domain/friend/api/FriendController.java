a5bf79d2ac75854036590d97a9dccb7311b5b8fe
문여경 <logbymoon@naver.com>
2023-03-30 17:44:05 +0900

    feature: 유저 랭킹

--- back/src/main/java/bunsan/returnz/domain/friend/api/FriendController.java
+++ back/src/main/java/bunsan/returnz/domain/friend/api/FriendController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import bunsan.returnz.domain.friend.dto.FriendInfo;
@@ -61,9 +62,9 @@ public class FriendController {
 	}
 
 	//----------------------------------------친구 삭제---------------------------------------------
-	@DeleteMapping("/api/friends/{username}")
+	@DeleteMapping("/api/friends")
 	public ResponseEntity deleteFriend(@RequestHeader(value = "Authorization") String bearerToken,
-		@PathVariable String username) {
+		@RequestParam String username) {
 		String token = bearerToken.substring(7);
 		friendService.deleteFriend(username, token);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
