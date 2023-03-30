e900c0ef182141e5f7015a859302ef4b6f22f0eb
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-03-30 17:26:23 +0900

    Feat: 친추 거절 기능

--- front/src/apis/friendApi.js
+++ front/src/apis/friendApi.js
@@ -1,10 +1,9 @@
 import { authApi } from './axiosConfig';
 
-const getFriendRequests = () => {
+const getFriendRequestsApi = () => {
   return authApi
     .get('requests')
     .then((res) => {
-      console.log(res.data.friendRequestList);
       return res.data.friendRequestList;
     })
     .catch((err) => {
@@ -12,4 +11,26 @@ const getFriendRequests = () => {
     });
 };
 
-export { getFriendRequests };
+const acceptFriendRequestApi = (payload) => {
+  return authApi
+    .post(`/friends/${payload}`)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+const declineFriendRequestApi = (payload) => {
+  return authApi
+    .delete(`/requests/${payload}`)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+export { getFriendRequestsApi, acceptFriendRequestApi, declineFriendRequestApi };
