2a121c1b39933f72e272673986b8f9733f2c3926
ats166 <ats166@naver.com>
2023-03-31 09:02:05 +0900

    feature : 종목소개
    Former-commit-id: 2b0ddd6f85f20aea7b0da8b8d157c201548d354d

--- front/src/store/gamedata/GameData.selector.js
+++ front/src/store/gamedata/GameData.selector.js
@@ -5,7 +5,9 @@ export const stockGraphList = (state) => state.gamedatas.stockGraphList;
 // 영업날 아닌 인덱스
 export const noWorkDay = (state) => state.gamedatas.noWorkDay;
 // 뉴스, 주가정보, 종목 내용
-export const stockDetailDataList = (state) => state.gamedatas.stockDetailDataList;
+export const stockNews = (state) => state.gamedatas.stockNews;
+export const stockInfomation = (state) => state.gamedatas.stockInfomation;
+export const stockdescription = (state) => state.gamedatas.stockdescription;
 // 보유 종목
 export const gamerStockList = (state) => state.gamedatas.gamerStockList;
 //  유저 데이터
