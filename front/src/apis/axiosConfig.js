de3657108dbf562062fca245ba5aaaebafa04027
ats166 <ats166@naver.com>
2023-03-31 09:47:35 +0900

    hotfix : 기능 병합
    Former-commit-id: 2486811faf6c1d04679779b379180f7ff3c4dd36

--- front/src/apis/axiosConfig.js
+++ front/src/apis/axiosConfig.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import StompJs from 'stompjs';
-import SockJs from 'sockjs-client';
-import Stomp from 'webstomp-client';
 
 // 로그인된 사용자
 const BASE_URL = 'http://j8c106.p.ssafy.io/api';
@@ -27,37 +24,5 @@ authApi.interceptors.request.use(
 const openApi = axios.create({
   baseURL: 'https://www.alphavantage.co/',
 });
-
-// 웹소켓 api
-// const wsApi = axios.create();
-// wsApi.defaults.baseURL = 'http://j8c106.p.ssafy.io:8188/ws';
-// wsApi.defaults.baseURL = 'http://192.168.100.175:8080/ws';
-// const wsApi = axios.create({
-//   baseURL: 'http://j8c106.p.ssafy.io:8188/ws',
-// });
-// const sock = new SockJs(wsApi);
-// // const sock = new SockJs('http://j8c106.p.ssafy.io:8188/ws');
-// const options = {
-//   debug: false,
-//   protocols: Stomp.VERSIONS.supportedProtocols(),
-// };
-// // client 객체 생성 및 서버주소 입력
-// const stomp = StompJs.over(sock, options);
-
-// 로그인 후 사용할 api (프사수정, 닉수정)
-const authApi = axios.create({
-  baseURL: BASE_URL,
-});
-
-authApi.interceptors.request.use(
-  (request) => {
-    const ACCESS_TOKEN = Cookies.get('access_token');
-    request.headers.Authorization = ACCESS_TOKEN || null;
-    return request;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
 
 export { axios, openApi, authApi };
