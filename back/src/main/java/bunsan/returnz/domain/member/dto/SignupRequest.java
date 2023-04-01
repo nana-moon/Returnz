920f5ed2e2b9e963e1773a74211d6548b5925241
문여경 <logbymoon@naver.com>
2023-04-01 20:26:42 +0900

    hotfix: 닉네임 유효성 검사 수정

--- back/src/main/java/bunsan/returnz/domain/member/dto/SignupRequest.java
+++ back/src/main/java/bunsan/returnz/domain/member/dto/SignupRequest.java
@@ -29,7 +29,7 @@ public class SignupRequest {
 	private String passwordConfirmation;
 
 	@NotBlank(message = "닉네임을 입력해주세요.")
-	@Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$",
+	@Pattern(regexp = "^(?=.*[A-Za-z0-9가-힣])[A-Za-z0-9가-힣]{2,16}$",
 		message = "닉네임은 2자 이상 16자 이하, 영어 또는 숫자 또는 한글이어야 합니다.")
 	@Size(min = 1, max = 10, message = "닉네임는 1자 이상 10자 이하여야 합니다.")
 	private String nickname;
