70c64a4e121b4cfe7d13d81d0083a0bb20c93357
nana-moon <yeokyung502@gmail.com>
2023-04-02 20:29:10 +0900

    hotfix: 대기방 소켓 setting 메세지 오류 수정

--- back/src/main/java/bunsan/returnz/domain/game/api/GameSocketController.java
+++ back/src/main/java/bunsan/returnz/domain/game/api/GameSocketController.java
@@ -1,8 +1,13 @@
 package bunsan.returnz.domain.game.api;
 
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bunsan.returnz.domain.game.dto.RoomMessageDto;
@@ -34,4 +39,15 @@ public class GameSocketController {
 			throw new BadRequestException("요청 타입이 올바르지 않습니다.");
 		}
 	}
+
+	// @GetMapping("/api/server-time")
+	// public ResponseEntity sendServerTime(Map request) {
+	// 	String gameRoomId = (String)request.get("roomId");
+	// 	if (gameRoomId == null) {
+	// 		throw new BadRequestException("게임룸 id를 입력해주세요.");
+	// 	} else {
+	// 		Date returnDate = gameSocketService.sendServerTime(gameRoomId);
+	// 		return ResponseEntity.ok(Map.of("returnTime", returnDate));
+	// 	}
+	// }
 }
