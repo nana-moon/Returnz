8e571f388039f36653f4cea32ecf2bf2c53c6e19
jiaekim <jiae1625@gmail.com>
2023-04-02 13:24:22 +0900

    hotfix: gameId
    Former-commit-id: fb64c239f3f052aac439fa67ce029b0b5a22fbe6

--- front/src/store/roominfo/GameRoom.reducer.js
+++ front/src/store/roominfo/GameRoom.reducer.js
@@ -4,12 +4,16 @@ import { createSlice } from '@reduxjs/toolkit';
 export const gameRoom = createSlice({
   name: 'gameRoom',
   initialState: {
+    id: null,
     roomId: null,
     captainName: null,
     gamerId: null,
     playerList: [],
   },
   reducers: {
+    setGameId(state, action) {
+      state.id = action.payload;
+    },
     setGameRoomId(state, action) {
       state.roomId = action.payload;
     },
@@ -28,5 +32,6 @@ export const gameRoom = createSlice({
   },
 });
 
-export const { setGameRoomId, setCaptainName, setGamerId, setPlayerList, removePlayerList } = gameRoom.actions;
+export const { setGameId, setGameRoomId, setCaptainName, setGamerId, setPlayerList, removePlayerList } =
+  gameRoom.actions;
 export default gameRoom;
