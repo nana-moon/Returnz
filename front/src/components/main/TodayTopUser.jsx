52f9ffc56bd8c67c954ae1452f43b59cb5d467a5
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-04-03 15:38:54 +0900

    Feat: 웹소켓 친구 초대
    Former-commit-id: e59aeb0289d8df34884fe10afce3316be7053109

--- front/src/components/main/TodayTopUser.jsx
+++ front/src/components/main/TodayTopUser.jsx
@@ -1,85 +1,100 @@
 import { React, useState, useEffect } from 'react';
 import tw, { styled } from 'twin.macro';
 import Chart from 'react-apexcharts';
-import TodayTopUserItem from './Items/TodayTopUserItem';
+// import TodayTopUserItem from './Items/TodayTopUserItem';
+import { useQuery } from 'react-query';
 import { getTopTenRank } from '../../apis/homeApi';
 
 export default function TodayTopUser() {
-  const [topTenUsers, setTopTenUsers] = useState([]);
+  // const [topTenUsers, setTopTenUsers] = useState([]);
+  const [chartData, setChartData] = useState(null);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await getTopTenRank();
+  //     setTopTenUsers(res);
+  //   }
+  //   fetchData();
+  // }, []);
+  // 내가 받은 친구요청 리스트
+  const { data: topTenUsers } = useQuery({
+    queryKey: ['topTenUsers'],
+    queryFn: () => getTopTenRank(),
+    onError: (e) => {
+      console.log(e);
+    },
+  });
   useEffect(() => {
-    async function fetchData() {
-      const res = await getTopTenRank();
-      console.log(res, '랭킹10명');
-      setTopTenUsers(res);
+    if (topTenUsers.length > 0) {
+      const userNicknames = topTenUsers.map((data) => data.nickname);
+      const userProfit = topTenUsers.map((data) => data.avgProfit);
+      if (userNicknames && userProfit) {
+        const chartOptions = {
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              borderRadius: 6,
+            },
+          },
+          fill: {
+            colors: ['#13ADA2'],
+            opacity: 0.8,
+          },
+          grid: {
+            show: false,
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          xaxis: {
+            title: {
+              text: '평균수익률',
+            },
+            categories: userNicknames,
+          },
+          labels: {
+            rotate: 45,
+          },
+          yaxis: {
+            title: {
+              text: '수익률 TOP 10',
+            },
+
+            // labels: {
+            //   // formatter(val) {
+            //   //   const user = topTenUsers[val - 1];
+            //   //   return `${user?.nickname}`;
+            //   // },
+            // },
+          },
+        };
+
+        const chartSeries = [
+          {
+            name: 'Profit',
+            data: userProfit,
+          },
+        ];
+
+        setChartData({ options: chartOptions, series: chartSeries });
+      }
     }
-    fetchData();
-  }, []);
-
-  const userNicknames = topTenUsers?.map((data) => data.nickname);
-  // const userProfile = topTenUsers?.map((data) => data.profileIcon);
-  const userProfit = topTenUsers?.map((data) => data.avgProfit);
-
-  const chartOptions = {
-    plotOptions: {
-      bar: {
-        horizontal: true,
-        borderRadius: 6,
-      },
-    },
-    fill: {
-      colors: ['#13ADA2'],
-      opacity: 0.8,
-    },
-    grid: {
-      show: false,
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    xaxis: {
-      title: {
-        text: '평균수익률',
-      },
-      categories: userNicknames,
-    },
-    labels: {
-      rotate: 45,
-    },
-    yaxis: {
-      title: {
-        text: '수익률 TOP 10',
-      },
-
-      // labels: {
-      //   // formatter(val) {
-      //   //   const user = topTenUsers[val - 1];
-      //   //   return `${user?.nickname}`;
-      //   // },
-      // },
-    },
-  };
-
-  const chartSeries = [
-    {
-      name: 'Profit',
-      data: userProfit,
-    },
-  ];
+  }, [topTenUsers]);
+  // const userNicknames = topTenUsers?.map((data) => data.nickname);
+  // // const userProfile = topTenUsers?.map((data) => data.profileIcon);
+  // const userProfit = topTenUsers?.map((data) => data.avgProfit);
 
   return (
     <TopUserContainer>
       <TopUserTitle>수익률 TOP 10</TopUserTitle>
-      <GraphContainer>
-        {userNicknames && userNicknames.length > 0 ? (
-          <Chart options={chartOptions} series={chartSeries} type="bar" />
-        ) : null}
-      </GraphContainer>
-      <TopUserBox>
+      {/* <GraphContainer>
+        {chartData && <Chart options={chartData.options} series={chartData.series} type="bar" />}
+      </GraphContainer> */}
+      {/* <TopUserBox>
         {topTenUsers?.map((person, i) => {
           // eslint-disable-next-line react/no-array-index-key
           return <TodayTopUserItem person={person} key={i} />;
         })}
-      </TopUserBox>
+      </TopUserBox> */}
     </TopUserContainer>
   );
 }
