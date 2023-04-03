52068b63020b417be3d0f69bd466a54221671829
ManduTheCat <krocd@naver.com>
2023-04-03 16:49:19 +0900

    feature : 함수 변경중
    Former-commit-id: 0911e165d112dfcca4673d1a6d16e5c24f4011c2

--- back/src/main/java/bunsan/returnz/persist/repository/DotcomThemeCompanyRepository.java
+++ back/src/main/java/bunsan/returnz/persist/repository/DotcomThemeCompanyRepository.java
@@ -1,6 +1,13 @@
 package bunsan.returnz.persist.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface DotcomThemeCompanyRepository extends JpaRepository<DotcomThemeCompanyRepository, String> {
+import bunsan.returnz.persist.entity.DotcomThemeCompany;
+
+public interface DotcomThemeCompanyRepository extends JpaRepository<DotcomThemeCompany, String> {
+	@Query(value = "SELECT d FROM DotcomThemeCompany d ORDER BY RAND()")
+	Page<DotcomThemeCompany> findRandomCompaniesId(Pageable pageable);
 }
