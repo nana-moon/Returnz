f11b8afc7b071fb49587345527974432dc114765
ManduTheCat <krocd@naver.com>
2023-04-03 16:18:54 +0900

    feature : add view entity, repository
    Former-commit-id: 2925ceb8557ba71ee27aad13e9b34db2ddc16e3a

---
+++ back/src/main/java/bunsan/returnz/persist/repository/DotcomThemeCompanyRepository.java
@@ -0,0 +1,6 @@
+package bunsan.returnz.persist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DotcomThemeCompanyRepository extends JpaRepository<DotcomThemeCompanyRepository, String> {
+}
