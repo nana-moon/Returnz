ed18393de94c32b80712a99c214ffe848301be0b
ManduTheCat <krocd@naver.com>
2023-04-03 16:18:54 +0900

    feature : add view entity, repository

---
+++ back/src/main/java/bunsan/returnz/persist/repository/RiemannThemeCompanyRepository.java
@@ -0,0 +1,8 @@
+package bunsan.returnz.persist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import bunsan.returnz.persist.entity.RiemannThemeCompany;
+
+public interface RiemannThemeCompanyRepository extends JpaRepository<RiemannThemeCompany, String> {
+}
