0fdb1edbfa9cd2846b1d30e60dac7dbffd30dd38
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-04-03 09:14:35 +0900

    Feat: 사이드바 웹소켓
    Former-commit-id: c794fc2812c94549c25bf8bc171af6214091d5fd

--- front/src/components/common/Items/FriendListItems.jsx
+++ front/src/components/common/Items/FriendListItems.jsx
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import SockJs from 'sockjs-client';
+import StompJs from 'stompjs';
+import Cookies from 'js-cookie';
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import tw, { styled } from 'twin.macro';
+import Stomp from 'webstomp-client';
 import { Avatar } from '@material-tailwind/react';
+import { getWaitRoomId } from '../../../store/roominfo/WaitRoom.selector';
 
 export default function FriendListItems({ friend }) {
+  // 내정보
+  const myToken = Cookies.get('access_token');
+  const [currentRoute, setCurrentRoute] = useState('');
+  const location = useLocation();
+  useEffect(() => {
+    setCurrentRoute(location.pathname);
+  }, [location.pathname]);
   const [friendColor, setfriendColor] = useState(false);
   const [friendStateColor, setfriendStateColor] = useState(false);
   useEffect(() => {
@@ -17,17 +31,52 @@ export default function FriendListItems({ friend }) {
       setfriendStateColor('text-negative');
     }
   }, []);
+  // 대기자 STATE
+  const roomId = useSelector(getWaitRoomId);
+  // 웹소켓
+  const sock = new SockJs('http://j8c106.p.ssafy.io:8188/ws');
+  const options = {
+    debug: false,
+    protocols: Stomp.VERSIONS.supportedProtocols(),
+  };
+  // client 객체 생성 및 서버주소 입력
+  const stomp = StompJs.over(sock, options);
+  const handleInviteRequest = () => {
+    stomp.send(
+      '/pub/side-bar',
+      {
+        // 여기에서 유효성 검증을 위해 header를 넣어줄 수 있음
+        Authorization: `${myToken}`,
+      },
+      JSON.stringify({
+        type: 'FRIEND',
+        messageBody: {
+          roomId: `${roomId}`,
+          username: `${friend.username}`,
+        },
+      }),
+    );
+  };
 
   return (
-    <FriendInfoContainer>
-      <Avatar variant="circular" src={`profile_pics/${friend.profileIcon}.jpg`} className="border-2 border-negative" />
-      <FriendStateCircle className={friendColor} />
-      <FriendInfoSection>
-        <FriendNameItem>{friend.nickname}</FriendNameItem>
-        {/* <FriendNameItem>열글자닉네임은어떨까</FriendNameItem> */}
-        <FriendStateItem className={friendStateColor}>{friend.state}</FriendStateItem>
-      </FriendInfoSection>
-    </FriendInfoContainer>
+    <>
+      <FriendInfoContainer>
+        <Avatar
+          variant="circular"
+          src={`profile_pics/${friend.profileIcon}.jpg`}
+          className="border-2 border-negative"
+        />
+        <FriendStateCircle className={friendColor} />
+        <FriendInfoSection>
+          {/* <FriendNameItem>{friend.nickname}</FriendNameItem> */}
+          <FriendNameItem>이름이열글자인사람이</FriendNameItem>
+          <FriendStateItem className={friendStateColor}>{friend.state}</FriendStateItem>
+        </FriendInfoSection>
+      </FriendInfoContainer>
+      {currentRoute === '/waiting' ? (
+        <FriendInviteButton onClick={handleInviteRequest}>게임 초대하기</FriendInviteButton>
+      ) : null}
+    </>
   );
 }
 
@@ -39,7 +88,7 @@ const FriendInfoSection = styled.div`
   ${tw`ml-3`}
 `;
 const FriendNameItem = styled.div`
-  ${tw`text-lg mt-1`}
+  ${tw`mt-1`}
 `;
 const FriendStateItem = styled.div`
   position: absolute;
@@ -53,4 +102,8 @@ const FriendStateCircle = styled.div`
   left: 2.8rem;
   top: 2.6rem;
   ${tw`rounded-full w-4 h-4`}
+`;
+
+const FriendInviteButton = styled.button`
+  ${tw`text-white bg-primary border-4 border-primary hover:bg-dprimary focus:border-dprimary font-bold text-sm rounded-lg px-3 py-1 text-center mx-10`};
 `;
--- front/src/utils/Socket.jsx
+++ front/src/utils/Socket.jsx
@@ -8,6 +8,9 @@ const sock = new SockJs('http://j8c106.p.ssafy.io:8188/ws');
 const options = {
   debug: false,
   protocols: Stomp.VERSIONS.supportedProtocols(),
+  reconnectDelay: 1000, // 재시도 간격 1초로 설정
+  heartbeatIncoming: 4000, // 4초마다 서버로부터 heartbeat 메시지 받기
+  heartbeatOutgoing: 4000, // 4초마다 서버로 heartbeat 메시지 보내기
 };
 
 // SOCKET MANAGER
@@ -16,8 +19,24 @@ const stomp = StompJs.over(sock, options);
 // CONNECT SOCKET
 const stompConnect = (header, socketAction) => {
   stomp.debug = null;
+  // console.log('connect');
+  stomp.onConnect = () => {
+    console.log('WebSocket connection opened');
+    // stomp.subscribe('/topic/example', socketAction); // 예시로 /topic/example 주제에 대한 구독
+  };
+
+  stomp.onStompError = (frame) => {
+    console.error('WebSocket error:', frame);
+  };
+
+  stomp.onWebSocketClose = () => {
+    console.log('WebSocket connection closed');
+    setTimeout(() => stompConnect(header, socketAction), stomp.reconnectDelay);
+  };
+  console.log('connect');
   stomp.connect(header, socketAction, (error) => {
     console.log('connection error:', error);
+    setTimeout(() => stompConnect(header, socketAction), stomp.reconnectDelay);
   });
 };
 
