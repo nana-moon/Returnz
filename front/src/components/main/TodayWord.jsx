42b83b982a2a549cca0f627f9e15d73fe93f862b
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-04-04 17:57:08 +0900

    Feat: 오늘의 단어, 뉴스
    Former-commit-id: 42e02b8fc34eb5251a654e0e2883647453c2dbe1

--- front/src/components/main/TodayWord.jsx
+++ front/src/components/main/TodayWord.jsx
@@ -27,40 +27,39 @@ export default function TodayWord() {
   }, []);
   return (
     <TodayWordContainer>
-      <TodayWordCarousel>
-        <WordTitle>{word[currentIndex]?.keyWord}</WordTitle>
-        <WordContent>{word[currentIndex]?.description}</WordContent>
-        <LeftArrow>
-          <BsFillArrowLeftCircleFill onClick={prevSlide} />
-        </LeftArrow>
-        <RightArrow>
-          <BsFillArrowRightCircleFill onClick={nextSlide} />
-        </RightArrow>
-      </TodayWordCarousel>
+      <TodayWordTitle> 📝 오늘의 단어</TodayWordTitle>
+      <WordTitle>{word[currentIndex]?.keyWord}</WordTitle>
+      <WordContent>{word[currentIndex]?.description}</WordContent>
+      <LeftArrow>
+        <BsFillArrowLeftCircleFill onClick={prevSlide} />
+      </LeftArrow>
+      <RightArrow>
+        <BsFillArrowRightCircleFill onClick={nextSlide} />
+      </RightArrow>
     </TodayWordContainer>
   );
 }
 
 const TodayWordContainer = styled.div`
-  ${tw`text-center p-2`}
-`;
-
-const TodayWordCarousel = styled.div`
-  ${tw`relative bg-white w-[100%] h-[100%] rounded-xl border-2 border-negative p-2`}
+  ${tw`text-center w-[100%] h-[70%] relative bg-white rounded-xl border-2 border-negative py-10 px-2 mt-12`}
 `;
 
 const WordTitle = styled.div`
   ${tw`text-2xl font-bold text-left mx-10 border-b-2 pb-2 mt-2`}
 `;
-
+const TodayWordTitle = styled.div`
+  transform: translateY(-56px);
+  left: 5%;
+  ${tw`text-3xl text-center font-bold border w-[90%] absolute bg-white rounded-2xl border-negative border-2 py-1`}
+`;
 const WordContent = styled.div`
   ${tw`text-sm ml-11 mr-10 mt-5 text-left indent-4`}
 `;
 
 const LeftArrow = styled.div`
-  ${tw`absolute top-12 translate-y-[50%] left-1 text-2xl cursor-pointer`}
+  ${tw`absolute top-[50%] translate-y-[50%] left-1 text-2xl cursor-pointer`}
 `;
 
 const RightArrow = styled.div`
-  ${tw`absolute top-12 translate-y-[50%] right-1 text-2xl cursor-pointer`}
+  ${tw`absolute top-[50%] translate-y-[50%] right-1 text-2xl cursor-pointer`}
 `;
