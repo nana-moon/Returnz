f1336e73917cf044bbba274f2f527ab4a97f0c2b
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-04-05 00:34:18 +0900

    Feat: state 메세지

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import tw, { styled } from 'twin.macro';
-import { Avatar } from '@material-tailwind/react';
 import TodayNews from '../components/main/TodayNews';
 import TodayPrice from '../components/main/TodayPrice';
 import TodayTopUser from '../components/main/TodayTopUser';
@@ -21,9 +20,6 @@ export default function HomePage() {
         <RecommendedSection>
           <TodayNews />
         </RecommendedSection>
-        {/* <InfoSection>
-          
-        </InfoSection> */}
         <InfoSection>
           <TodayWord />
         </InfoSection>
@@ -37,7 +33,7 @@ const MainContainer = styled.div`
 `;
 const PriceSection = styled.div`
   height: 56px;
-  ${tw``}
+  ${tw`mt-2`}
 `;
 
 const ButtonsSection = styled.div`
@@ -45,9 +41,6 @@ const ButtonsSection = styled.div`
 `;
 const RecommendedSection = styled.div`
   ${tw`col-span-3`}
-`;
-const TestBox = styled.div`
-  ${tw`bg-white border-2 border-negative rounded-lg`}
 `;
 
 const TopUsersSection = styled.div`
