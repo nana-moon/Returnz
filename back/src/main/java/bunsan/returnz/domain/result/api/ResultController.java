978b0c8e910a7b04eb17f291e5195a993a624b0b
Fortuna3Co <Fortuna3Co@gmail.com>
2023-04-04 14:11:23 +0900

    feature : 1. game_room Entity 변경으로 인한  매수, 매도 날짜 변경
    Former-commit-id: f74fe939c568a3382e233bb586df994067336da9

--- back/src/main/java/bunsan/returnz/domain/result/api/ResultController.java
+++ back/src/main/java/bunsan/returnz/domain/result/api/ResultController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bunsan.returnz.domain.game.dto.GameGamerDto;
-import bunsan.returnz.domain.game.dto.PurchaseSaleLogDto;
 import bunsan.returnz.domain.game.service.GameRoomService;
 import bunsan.returnz.domain.game.service.GamerService;
 import bunsan.returnz.domain.member.service.MemberService;
 import bunsan.returnz.domain.result.dto.GamerLogResponseDto;
+import bunsan.returnz.domain.result.dto.PurchaseSaleLogResponseDto;
 import bunsan.returnz.domain.result.dto.ResultRequestBody;
 import bunsan.returnz.domain.result.service.ResultService;
 import bunsan.returnz.global.advice.exception.BadRequestException;
@@ -53,7 +53,7 @@ public class ResultController {
 		for (int i = 0; i < gameGamerDtos.size(); ++i) {
 			HashMap<String, Object> gamerInformation = new HashMap<>();
 
-			List<PurchaseSaleLogDto> purchaseSaleLogDtos
+			List<PurchaseSaleLogResponseDto> purchaseSaleLogResponseDtos
 				= resultService.findAllByGameRoomIdAndMemberIdOrderById(
 				resultRequestBody.getGameRoomId(), gameGamerDtos.get(i).getMermberId()
 			);
@@ -68,7 +68,7 @@ public class ResultController {
 			gamerInformation.put("nickname", member.getNickname());
 			gamerInformation.put("profile", member.getProfileIcon());
 			gamerInformation.put("prev_avg_profit", gameGamerDtos.get(i).getTotalProfitRate());
-			gamerInformation.put("trade_list", purchaseSaleLogDtos);
+			gamerInformation.put("trade_list", purchaseSaleLogResponseDtos);
 			gamerInformation.put("profits", gamerLogResponseDtos);
 			responseInformation.add(gamerInformation);
 		}
