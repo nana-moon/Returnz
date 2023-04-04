2c67064c5516f3550abdcfd107cf53725b3495ec
Fortuna3Co <Fortuna3Co@gmail.com>
2023-04-04 19:31:15 +0900

    feature : 1. 보유 종목 버그 수정
    Former-commit-id: 55cc3cf051dcffb49b7cb7283644f316b3696bc0

--- back/src/main/java/bunsan/returnz/domain/game/service/GameService.java
+++ back/src/main/java/bunsan/returnz/domain/game/service/GameService.java
@@ -93,6 +93,21 @@ public class GameService {
 		// 3. 나의 현재 보유 종목, companyName과 logo 붙여서 return
 		log.info("================= turn progress - 나의 보유 종목");
 		List<GameGamerStockDto> gameGamerStockDtos = gamerStockService.findAllByGamer_Id(gamerId);
+
+		// 6. 주가 정보
+		turnInformation.put("stockInformation", getStockPriceInformation(gameRoomDto.getCurDate(), gameStockDtoList));
+
+		//  7. 첫턴일 떄만 종목 디테일 정보 제공
+		if (gameRoomDto.getCurTurn() == 0) {
+			turnInformation.put("companyDetail", getCompanyDetail(gameStockDtoList));
+		}
+
+		// 4. 다음 턴 정보 업데이트
+		if (!updateTurnInformation(gameGamerStockDtos, roomId,
+			gameRoomDto, gamerId)) {
+			throw new BusinessException("다음 턴 정보를 얻어올 수 없습니다.");
+		}
+
 		// 주식 평가 정보 update
 		updateStockInformation(gameGamerStockDtos, roomId,
 			gameRoomDto, gamerId);
@@ -109,20 +124,6 @@ public class GameService {
 		}
 
 		turnInformation.put("gamerStock", gameGamerStockDtosResponse);
-
-		// 6. 주가 정보
-		turnInformation.put("stockInformation", getStockPriceInformation(gameRoomDto.getCurDate(), gameStockDtoList));
-
-		//  7. 첫턴일 떄만 종목 디테일 정보 제공
-		if (gameRoomDto.getCurTurn() == 0) {
-			turnInformation.put("companyDetail", getCompanyDetail(gameStockDtoList));
-		}
-
-		// 4. 다음 턴 정보 업데이트
-		if (!updateTurnInformation(gameGamerStockDtos, roomId,
-			gameRoomDto, gamerId)) {
-			throw new BusinessException("다음 턴 정보를 얻어올 수 없습니다.");
-		}
 
 		return turnInformation;
 	}
