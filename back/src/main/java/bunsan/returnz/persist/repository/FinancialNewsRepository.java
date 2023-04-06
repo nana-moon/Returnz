cf03067900bab5a652c8f3730818772681ccff6a
ManduTheCat <krocd@naver.com>
2023-04-06 12:22:01 +0900

    feature : theme 시연용변경 이후 턴 이동 확인

--- back/src/main/java/bunsan/returnz/persist/repository/FinancialNewsRepository.java
+++ back/src/main/java/bunsan/returnz/persist/repository/FinancialNewsRepository.java
@@ -17,11 +17,14 @@ public interface FinancialNewsRepository extends JpaRepository<FinancialNews, Lo
 	List<LocalDateTime> findDistinctDatesAfter(@Param("startDate") LocalDateTime startDate, Pageable pageable,
 		@Param("companyCodes") List<String> companyCodes);
 
-	@Query(value = "SELECT fn FROM FinancialNews fn "
+	@Query(value = "SELECT "
+		+ "DISTINCT fn.date, fn"
+		+ " FROM FinancialNews fn "
 		+ "WHERE fn.date between :startDate And :endDate "
 		+ "AND fn.code IN :companyCodes"
 		+ " ORDER BY fn.date ASC")
-	List<FinancialNews> findAllByDateAndCompanyCodes(@Param("startDate") LocalDateTime startDate,
+	List<Object[]> findAllByDateAndCompanyCodes(@Param("startDate") LocalDateTime startDate,
 		@Param("endDate") LocalDateTime endDate,
 		@Param("companyCodes") List<String> companyCodes);
+
 }
